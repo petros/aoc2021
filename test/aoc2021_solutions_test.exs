d223bdc434a886163d2406c83d1813c0fd94fde0
petros <petros@amiridis.net>
2021-12-11 15:01:35 +0200

    Solve day one

---
+++ test/aoc2021_solutions_test.exs
@@ -0,0 +1,4 @@
+defmodule Aoc2021SolutionsTest do
+  use ExUnit.Case
+  doctest Aoc2021Solutions
+end
--- test/aoc2021_test.exs
+++ test/aoc2021_test.exs
@@ -2,7 +2,7 @@ defmodule Aoc2021Test do
   use ExUnit.Case
   doctest Aoc2021
 
-  test "greets the world" do
-    assert Aoc2021.hello() == :world
-  end
+  #test "greets the world" do
+  #  assert Aoc2021.hello() == :world
+  #end
 end
