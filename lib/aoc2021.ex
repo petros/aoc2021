4813b30a9678b2ec465b5b8265fa1056e3836420
Petros Amiridis <petros@amiridis.net>
2021-12-18 17:01:24 +0200

    Start day two

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -4,13 +4,13 @@ defmodule Aoc2021 do
   function has doctests that solve the example input. So no spoilers in this
   module. Other than the actual code that solves the puzzle of course. The code
   can definitely be better as I am a novice Elixir coder.
-  
+
   [1]: https://adventofcode.com
   """
 
   @doc """
   Day one. Figure out how quickly the depth increases.
-  
+
   As the submarine drops below the surface of the ocean, it automatically
   performs a sonar sweep of the nearby sea floor. On a small screen, the sonar
   sweep report (your puzzle input) appears: each line is a measurement of the
@@ -77,7 +77,7 @@ defmodule Aoc2021 do
     |> Enum.filter(fn [l, r] -> l < r end)
     |> Enum.count
   end
-  
+
   @doc """
   Day one part two. Figure out how quickly the depth increases but this time
   using a three-measurement sliding window to reduce noise in the data.
@@ -129,7 +129,7 @@ defmodule Aoc2021 do
 
   Consider sums of a three-measurement sliding window. How many sums are larger
   than the previous sum?
-  
+
   ## Parameters
 
     - depths: List that containts sonar depth measurements
@@ -145,5 +145,52 @@ defmodule Aoc2021 do
     |> Enum.chunk_every(3, 1, :discard)
     |> Enum.map(fn(tmw) -> Enum.sum(tmw) end)
     |> Aoc2021.day_one
+  end
+
+  @doc """
+  --- Day 2: Dive! ---
+
+  Now, you need to figure out how to pilot this thing.
+
+  It seems like the submarine can take a series of commands like `forward 1`,
+  `down 2`, or `up 3`:
+
+  - `forward X` increases the horizontal position by `X` units.
+  - `down X` increases the depth by `X` units.
+  - `up X` decreases the depth by `X` units.
+
+  Note that since you're on a submarine, down and up affect your **depth**, and
+  so they have the opposite result of what you might expect.
+
+  The submarine seems to already have a planned course (your puzzle input).
+  You should probably figure out where it's going. For example:
+
+  ```
+  forward 5
+  down 5
+  forward 8
+  up 3
+  down 8
+  forward 2
+  ```
+
+  Your horizontal position and depth both start at `0`. The steps above would then
+  modify them as follows:
+
+  - `forward 5` adds `5` to your horizontal position, a total of `5`.
+  - `down 5` adds `5` to your depth, resulting in a value of `5`.
+  - `forward 8` adds `8` to your horizontal position, a total of `13`.
+  - `up 3` decreases your depth by `3`, resulting in a value of `2`.
+  - `down 8` adds `8` to your depth, resulting in a value of `10`.
+  - `forward 2` adds `2` to your horizontal position, a total of `15`.
+
+  After following these instructions you would have a horizontal position of
+  `15` and a depth of `10`. (Multiplying these together produces **`150`**.)
+
+  Calculate the horizontal position and depth you woulda have after following
+  the planned course. **What do you get if you multiply your final horizontal
+  position by your final depth?**
+  """
+  def day_two() do
   end
 end
