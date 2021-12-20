5a2ae1da5382ed9f157dbcc2074c7198ee70fc89
Petros Amiridis <petros@amiridis.net>
2021-12-20 09:15:11 +0200

    Fix typos

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -63,7 +63,7 @@ defmodule Aoc2021 do
 
   ## Parameters
 
-    - depths: List that containts sonar depth measurements
+    - depths: List that contains sonar depth measurements
 
   ## Examples
 
@@ -132,7 +132,7 @@ defmodule Aoc2021 do
 
   ## Parameters
 
-    - depths: List that containts sonar depth measurements
+    - depths: List that contains sonar depth measurements
 
   ## Examples
 
@@ -187,7 +187,7 @@ defmodule Aoc2021 do
   After following these instructions you would have a horizontal position of
   `15` and a depth of `10`. (Multiplying these together produces **`150`**.)
 
-  Calculate the horizontal position and depth you woulda have after following
+  Calculate the horizontal position and depth you would have after following
   the planned course. **What do you get if you multiply your final horizontal
   position by your final depth?**
 
